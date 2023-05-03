8d093ff319e0159a65a642e365ffaf266b612bb8
mean1687 <mean1687@gmail.com>
2023-05-03 13:04:42 +0900

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace cv;
+using namespace std;
+
+int main(int ac, char** av) {
+	Mat img = imread("image/Lenna.png");
+
+	imshow("img", img);
+	waitKey(0);
+
+	return 0;
+}
